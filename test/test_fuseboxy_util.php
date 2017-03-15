a3e84306c225ea8f146a182c21b023a1c04dbfc5
Henry Shum <henry@metaseit.com>
2017-03-15 14:57:57 +0800

    empty unit-test skeleton and empty readme

---
+++ test/test_fuseboxy_util.php
@@ -0,0 +1,26 @@
+<?php
+class TestFuseboxyUtil extends UnitTestCase {
+
+
+	function __construct() {}
+
+
+	function test__decrypt() {}
+
+
+	function test__encrypt() {}
+
+
+	function test__html2text() {}
+
+
+	function test__minifyHtml() {}
+
+
+	function test__sendMail() {}
+
+
+	function test__xslt() {}
+
+
+} // TestFuseboxyUtil
