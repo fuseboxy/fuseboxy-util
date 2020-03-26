53580802559ab6c94f76f00eaf277c87d7997440
HENRY SHUM <henrygotmojo@gmail.com>
2020-03-26 22:00:55 +0800

    remove unit-test directory

--- test/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-set_time_limit(0);
-require_once('simpletest/1.1.0/autorun.php');
-foreach ( glob("test_fuseboxy_*.php") as $file ) include $file;
--- test/utility-util/config/fusebox_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array(
-	'defaultCommand' => 'site',
-	'appPath' => dirname(dirname(dirname(__DIR__))).'/app',
-);
