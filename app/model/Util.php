163386392110a4cbec4264f4887be319023c97f5
henrygotmojo <henrygotmojo@gmail.com>
2022-09-30 17:38:49 +0800

    make array2pdf & html2pdf default rendering pdf directly

--- app/model/Util.php
+++ app/model/Util.php
@@ -47,7 +47,7 @@ class Util {
 		<io>
 			<in>
 				<array name="$fileData" />
-				<string name="$filePath" optional="yes" default="~emptyString~" comments="relative path to upload directory; use {false} or {null} to display PDF directly" />
+				<string name="$filePath" optional="yes" default="~null~" comments="relative path to upload directory; use {false} or {null} to display PDF directly" />
 				<structure name="$options" optional="yes" />
 			</in>
 			<out>
@@ -62,7 +62,7 @@ class Util {
 		</io>
 	</fusedoc>
 	*/
-	public static function array2pdf($fileData, $filePath='', $options=[]) {
+	public static function array2pdf($fileData, $filePath=null, $options=[]) {
 		// validate library
 		foreach ( self::$libPath['array2pdf'] as $libClass ) {
 			if ( !class_exists($libClass) ) {
@@ -409,7 +409,7 @@ class Util {
 			<in>
 				<!-- parameters -->
 				<string name="$html" />
-				<string name="$filePath" optional="yes" default="~emptyString~" comments="relative path to upload directory; use {false} or {null} to display PDF directly" />
+				<string name="$filePath" optional="yes" default="~null~" comments="relative path to upload directory; use {false} or {null} to display PDF directly" />
 				<structure name="$pageOptions" optional="yes" />
 			</in>
 			<out>
@@ -424,7 +424,7 @@ class Util {
 		</io>
 	</fusedoc>
 	*/
-	public static function html2pdf($html, $filePath='', $options=[]) {
+	public static function html2pdf($html, $filePath=null, $options=[]) {
 		// validate library
 		foreach ( self::$libPath['html2pdf'] as $libClass ) {
 			if ( !class_exists($libClass) ) {
